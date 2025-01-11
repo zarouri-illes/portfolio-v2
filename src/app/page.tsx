639b0f1d5db5ba7c0a19ff17e817985d1789d9a3
illes <illeszarouri@gmail.com>
2025-01-11 22:58:28 +0100

    finished the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Header } from "@/sections/Header";
+
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Home page</h1>
     </div>
   );
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+    return (
+        <div className="flex justify-center items-center fixed top-0 w-full">
+            <nav className="flex gap-1 p-0.5 border-white/15 border rounded-t-none bg-white/10 backdrop-blur rounded-[10px]">
+                <a href="#" className="nav-item">Home</a>
+                <a href="#" className="nav-item">Home</a>
+                <a href="#" className="nav-item">Home</a>
+                <a href="#" className="nav-item">Home</a>
+            </nav>
+        </div>
+    )
 };
